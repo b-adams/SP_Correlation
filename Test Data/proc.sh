940dc1d2c1fd699e2bba441b88c6f5c88819b954
Bryant E Adams <badams@wells.edu>
2012-05-21 14:55:36 -0400

    Add basic test script

---
+++ Test Data/proc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./process 10000  sample_data/Phys438F1.txt sample_data/Com438F1.txt sample_data/Phys438F2.txt sample_data/Com438F2.txt sample_data/Phys438F3.txt sample_data/Com438F3.txt sample_data/Phys438F4.txt sample_data/Com438F4.txt sample_data/Phys438F5.txt sample_data/Com438F5.txt sample_data/Phys438F6.txt sample_data/Com438F6.txt sample_data/Phys438F7.txt sample_data/Com438F7.txt
